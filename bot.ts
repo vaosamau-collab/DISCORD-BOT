abc2a4e5d1e234de8e9fd50dd40b161ad1d913ca
vaosamau-collab <vaosamau@gmail.com>
2026-05-25 13:29:53 +0300

    Update bot.js

--- bot.ts
+++ bot.ts
@@ -1,32 +1,22 @@
 // @ts-nocheck
-import { Client, GatewayIntentBits, Events } from 'discord.js';
+const { Client, GatewayIntentBits, Events } = require('discord.js');
 
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent,
-        GatewayIntentBits.GuildMembers
+        GatewayIntentBits.MessageContent
     ]
 });
 
-client.on(Events.ClientReady, () => {
-    console.log(`البوت شغال يا أسامة!`);
+client.once(Events.ClientReady, () => {
+    console.log('البوت شغال يا أسامة!');
 });
 
-// هنا أوامرك القديمة
-client.on(Events.GuildMemberAdd, async (member) => {
-    const channel = member.guild.channels.cache.get("1508087523820310578");
-    if (channel && 'send' in channel) {
-        channel.send(`🎉 هلا والله ${member} نورت السيرفر! أنت العضو رقم ${member.guild.memberCount} في ${member.guild.name} 🏆`);
-    }
-});
-
-client.on(Events.MessageCreate, async (message) => {
+client.on(Events.MessageCreate, (message) => {
     if (message.content === '!ping') {
         message.reply('Pong!');
     }
-    // يمكنك إضافة أوامر التحذير وغيرها هنا بنفس طريقة الـ if
 });
 
 client.login(process.env.DISCORD_TOKEN);
